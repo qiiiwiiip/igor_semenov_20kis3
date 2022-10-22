9b18d464b251d6099b3fdb9c80fa63c3ca99e96f
User <sorus10q@yandex.ru>
2022-10-22 12:16:26 +0300

    did

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 print("semenov_igor")
 print("20kis3")
 print ("sleepy")
+
+n = int(input("enter the number: "))
+
+f1 = 1
+f2 = 1
+fs = 0
+
+n = int(n)
+
+i = 0
+while i < n - 2:
+    fs = f1 + f2
+    f1 = f2
+    f2 = fs
+    i = i + 1
+
+print(f2)
